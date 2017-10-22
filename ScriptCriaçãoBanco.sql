56b5e4b88919d111f0516ba25f75e57ff8f3d3bf
DESKTOP-TINC0KG\igor2 <cunha.981@gmail.com>
2017-10-22 04:42:15 -0200

    sql

--- ScriptCriaçãoBanco.sql
+++ ScriptCriaçãoBanco.sql
@@ -9,21 +9,26 @@ CREATE TABLE Usuario(
  ID INT not null IDENTITY(1,1) PRIMARY KEY,
  CPF VARCHAR (11) NOT NULL,
  Senha VARCHAR (20) NOT NULL,
+ email VARCHAR(30)
 )
 
 CREATE TABLE Pessoa(
  ID INT not null IDENTITY(1,1) PRIMARY KEY,
  CPF VARCHAR (11) NOT NULL,
- vl_DividaFixa INT NOT NULL,
- vl_Gastos INT NOT NULL,
+ situacao VARCHAR(30),
+ renda float,
+ saldo float,
+ vl_GastosFixo float NOT NULL,
+ vl_Gastos float NOT NULL,
+ vl_DividaTotal float NOT NULL,	
  Parcelas INT,
- vl_Parcelas INT
+ vl_Parcelas float
 )
 
 --INSERT NAS TABELAS
 
 USE INADIMPLENCIA
 
-INSERT INTO Usuario VALUES ('1',1);
+INSERT INTO Usuario VALUES ('1',1,'igor.almeida@verity.com.br');
 
 INSERT INTO Pessoa VALUES ('1',1500,500,null,null);
